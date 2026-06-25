bff3acf411a1642807d0b66a9fc56480193980d0
Han Yujin <hanyj224@hanyang.ac.kr>
2026-06-25 22:13:00 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        new = sorted(array[commands[i][0]-1:commands[i][1]])
+        answer.append(new[commands[i][2]-1])
+        
+    return answer
