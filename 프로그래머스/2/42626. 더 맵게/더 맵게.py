3da463401a30fdb3b850b7c5affc02254b974b7f
Han Yujin <hanyj224@hanyang.ac.kr>
2026-09-20 11:43:54 +0900

    [level 2] Title: 더 맵게, Time: 1704.66 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,11 +1,12 @@
 def solution(scoville, K):
     import heapq
-    answer = 0
     heapq.heapify(scoville)
-    while scoville[0] < K and len(scoville) >= 2:
+    
+    count = 0
+    while len(scoville) >= 2 and scoville[0] < K:
         min1 = heapq.heappop(scoville)
         min2 = heapq.heappop(scoville)
         heapq.heappush(scoville, min1 + min2 * 2)
-        answer += 1
+        count += 1
         
-    return answer if scoville[0] >= K else -1
+    return count if scoville[0] >= K else -1
