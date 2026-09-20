a54aecad3629d0c02206dd5658b6b978041b2a07
Han Yujin <hanyj224@hanyang.ac.kr>
2026-09-20 09:45:27 +0900

    [level 2] Title: 주식가격, Time: 18.90 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,13 +1,15 @@
 def solution(prices):
-    answer = []
-    sec = 1
-    price = prices[0]
-    for i in range(len(prices)):
-        sec = 0
-        for j in range(i+1, len(prices)):
-            sec += 1
-            if prices[j] < prices[i]:
-                break
-        answer.append(sec)
+    answer = [0] * len(prices)
+    stack = []
+    
+    for i, price in enumerate(prices):
+        while stack and prices[stack[-1]] > price:
+            idx = stack.pop()
+            answer[idx] = i - idx
+            
+        stack.append(i)
         
+    for idx in stack:
+        answer[idx] = len(prices) - 1 - idx
     return answer
+    
